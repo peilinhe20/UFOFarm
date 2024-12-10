a1b25ca3f7c5ddba1b7d674e155e3fc26cdc3e47
Zhiyuan Gu <87624521+Dotagoodgogo@users.noreply.github.com>
2024-12-10 14:56:27 -0800

    add scenetransition function

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+    private int totalScenes;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start(){
+        totalScenes = SceneManager.sceneCountInBuildSettings;
+    }
+
+    void Update() {
+        CheatKeyToNextScene();
+    }
+
+    public void LoadNextScene () {
+        int index = SceneManager.GetActiveScene().buildIndex;
+
+        if(index + 1 < totalScenes) {
+            SceneManager.LoadScene(index + 1);
+        }
+        else{
+            //quit the game or ???
+        }        
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Additional logic can be added here if needed when a scene is loaded
+    }
+
+    private void CheatKeyToNextScene() {
+        if(Input.GetKeyDown(KeyCode.K)) {
+            LoadNextScene();
+        }
+    }
+}
