5655e38904d579c3f1bd5ab671c3bab6a3e05b95
peilin20 <Peilinhe@usc.edu>
2024-12-10 11:21:50 -0800

    revert to old version of control

--- Assets/Scripts/UFOcontrol.cs
+++ Assets/Scripts/UFOcontrol.cs
@@ -1,19 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class UFOcontrol : MonoBehaviour
 {
     public float moveSpeed = 5f;
-
-    private GameObject Ctrl;
-    SerialReceive Recieve;
-
     void Update()
     {
-        Ctrl = GameObject.Find("Serial_test");
-        Recieve = Ctrl.GetComponent<SerialReceive>();
-        float moveX = Recieve.A;
+        float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
         transform.Translate(new Vector3(moveX, moveY, 0) * moveSpeed * Time.deltaTime);
     }
