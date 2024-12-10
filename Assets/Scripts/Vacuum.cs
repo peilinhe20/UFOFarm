b3b8f348391c744354c0d052a81892eb40b652a0
peilin20 <Peilinhe@usc.edu>
2024-12-10 11:55:26 -0800

    working on dispensing animals mechanic

--- Assets/Scripts/Vacuum.cs
+++ Assets/Scripts/Vacuum.cs
@@ -11,17 +11,26 @@ public class Vacuum : MonoBehaviour
     public List<AnimalPair> correctPairs;
     private AudioSource vacuumAudio;
     private List<string> vacuumedAnimals = new List<string>();
-
+    private Queue<string> combinedAnimalsQueue = new Queue<string>();
     [System.Serializable]
     public struct AnimalPair
     {
         public string animal1;
         public string animal2;
+        public string combinedAnimalName;
     }
     void Start()
     {
         // Get the AudioSource component attached to the object
         vacuumAudio = GetComponent<AudioSource>();
+    }
+    private void Update()
+    {
+        // Check if the player presses the E key
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            InstantiateAnimal();
+        }
     }
     private void OnTriggerStay2D(Collider2D other)
     {
@@ -48,6 +57,14 @@ public class Vacuum : MonoBehaviour
             Debug.Log("vacuumed animal added!");
             // Check for a valid pair
             CheckForPair(animalName);
+            if (vacuumedAnimals.Count > 0)
+            {
+                for (int i = 0; i < vacuumedAnimals.Count; i++)
+                {
+                    combinedAnimalsQueue.Enqueue(vacuumedAnimals[i]);
+                }
+            }
+
             // Calculate direction toward the UFO
             Vector3 direction = (transform.position - other.transform.position).normalized;
 
@@ -69,12 +86,14 @@ public class Vacuum : MonoBehaviour
                 (pair.animal2 == newAnimal && vacuumedAnimals.Contains(pair.animal1)))
             {
                 CombineAnimals(pair.animal1, pair.animal2);
-
                 // Remove the combined animals from the list
                 vacuumedAnimals.Remove(pair.animal1);
                 vacuumedAnimals.Remove(pair.animal2);
-
                 break;
+            }
+            else
+            {
+
             }
         }
     }
@@ -86,15 +105,32 @@ public class Vacuum : MonoBehaviour
         if ( (animal1 == "Pig" && animal2 == "Cloud") ||
             (animal1 == "Cloud" && animal2 == "Pig"))
         {
-            string prefabPath = "Prefab/" + "cloudPig"; 
-            GameObject combinedAnimalPrefab = Resources.Load<GameObject>(prefabPath);
-            if (combinedAnimalPrefab != null)
-            {
-                GameObject combinedAnimal = Instantiate(combinedAnimalPrefab, vacuumZoneCenter, Quaternion.identity);
-                Debug.Log("Cloudpig is here!!");
-            }
+            combinedAnimalsQueue.Enqueue("cloudPig");
+            //string prefabPath = "Prefab/" + "cloudPig"; 
+            //GameObject combinedAnimalPrefab = Resources.Load<GameObject>(prefabPath);
+            //if (combinedAnimalPrefab != null)
+            //{
+            //    GameObject combinedAnimal = Instantiate(combinedAnimalPrefab, vacuumZoneCenter, Quaternion.identity);
+            //    Debug.Log("Cloudpig is here!!");
+            //}
 
         }
 
+    }
+
+    public void InstantiateAnimal()
+    {
+        if (combinedAnimalsQueue.Count > 0)
+        {
+            Debug.Log("dispensing...");
+            string nextAnimal = "Prefab/" + combinedAnimalsQueue.Dequeue();
+            GameObject AnimalPrefab = Resources.Load<GameObject>(nextAnimal);
+            Vector3 vacuumZoneCenter = GetComponent<Collider2D>().bounds.center;
+            if (AnimalPrefab != null)
+            {
+                GameObject combinedAnimal = Instantiate(AnimalPrefab, vacuumZoneCenter, Quaternion.identity);
+                Debug.Log("combined animal is here");
+            }
+        }
     }
 }
