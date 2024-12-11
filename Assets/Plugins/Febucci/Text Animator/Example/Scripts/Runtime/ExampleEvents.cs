0cbd7fbb52feaa607b4fcbacadb9ce3dd7316572
Zhiyuan Gu <87624521+Dotagoodgogo@users.noreply.github.com>
2024-12-10 17:02:33 -0800

    Update ExampleEvents.cs

--- Assets/Plugins/Febucci/Text Animator/Example/Scripts/Runtime/ExampleEvents.cs
+++ Assets/Plugins/Febucci/Text Animator/Example/Scripts/Runtime/ExampleEvents.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 namespace Febucci.UI.Examples
 {
     [AddComponentMenu(""), DisallowMultipleComponent]
-    class ExampleEvents : MonoBehaviour
+    public class ExampleEvents : MonoBehaviour
     {
         // ---- PART OF THE SCRIPT THAT YOU'RE PROBABLY INTERESTED IT ----
         private AudioSource audioSource;
@@ -135,6 +135,7 @@ namespace Febucci.UI.Examples
             else
             {
                 typewriter.StartDisappearingText();
+                this.enabled = false;
             }
         }
 
